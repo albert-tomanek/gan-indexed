f8d5454dda07400b4f1599fad8cfb8608be58980
Albert Tomanek <electron826@gmail.com>
2019-05-01 14:21:02 +0200

    Got it working for indexed images.

--- dcgan.py
+++ dcgan.py
@@ -1,26 +1,29 @@
 from __future__ import print_function, division
 
-from keras.datasets import mnist
-from keras.layers import Input, Dense, Reshape, Flatten, Dropout
+from keras.layers import Input, Dense, Permute, Reshape, Flatten, Dropout
 from keras.layers import BatchNormalization, Activation, ZeroPadding2D
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.models import Sequential, Model
 from keras.optimizers import Adam
 
-import matplotlib.pyplot as plt
+from keras.datasets import fashion_mnist
+import keras
+from keras import backend as K
 
+import matplotlib.pyplot as plt
+import numpy as np
 import sys
 
-import numpy as np
+from indexed_img import *
 
 class DCGAN():
     def __init__(self):
         # Input shape
         self.img_rows = 28
         self.img_cols = 28
-        self.channels = 1
-        self.img_shape = (self.img_rows, self.img_cols, self.channels)
+        self.num_colors = 3
+        self.img_shape = (self.img_rows, self.img_cols, self.num_colors)
         self.latent_dim = 100
 
         optimizer = Adam(0.0002, 0.5)
@@ -63,8 +66,9 @@ class DCGAN():
         model.add(Conv2D(64, kernel_size=3, padding="same"))
         model.add(BatchNormalization(momentum=0.8))
         model.add(Activation("relu"))
-        model.add(Conv2D(self.channels, kernel_size=3, padding="same"))
-        model.add(Activation("tanh"))
+        model.add(Conv2D(self.num_colors, kernel_size=3, padding="same"))   # (3, 28, 28)
+        model.add(Activation("sigmoid"))
+        # model.add(Permute(()))
 
         model.summary()
 
@@ -106,11 +110,11 @@ class DCGAN():
     def train(self, epochs, batch_size=128, save_interval=50):
 
         # Load the dataset
-        (X_train, _), (_, _) = mnist.load_data()
+        (x_data, _), (_, _) = fashion_mnist.load_data()
 
-        # Rescale -1 to 1
-        X_train = X_train / 127.5 - 1.
-        X_train = np.expand_dims(X_train, axis=3)
+        # Convert to grayscale indexed [0, 1, 2]
+        x_data = x_data // 86                                 # Splits colors in 0-255 plain into 3 more-less equaly sized parts.
+        x_data = keras.utils.to_categorical(x_data)     # One-hot encode
 
         # Adversarial ground truths
         valid = np.ones((batch_size, 1))
@@ -123,16 +127,16 @@ class DCGAN():
             # ---------------------
 
             # Select a random half of images
-            idx = np.random.randint(0, X_train.shape[0], batch_size)
-            imgs = X_train[idx]
+            idx = np.random.randint(0, x_data.shape[0], batch_size)
+            imgs = x_data[idx]
 
             # Sample noise and generate a batch of new images
             noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
             gen_imgs = self.generator.predict(noise)
 
             # Train the discriminator (real classified as ones and generated as zeros)
-            d_loss_real = self.discriminator.train_on_batch(imgs, valid)
-            d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
+            d_loss_real = self.discriminator.train_on_batch(imgs, valid)        # Teach that these images are valid
+            d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)     # Teach that these images are fake.
             d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
 
             # ---------------------
@@ -155,7 +159,9 @@ class DCGAN():
         gen_imgs = self.generator.predict(noise)
 
         # Rescale images 0 - 1
-        gen_imgs = 0.5 * gen_imgs + 0.5
+        gen_imgs = idx_to_rgb(onehot_to_indexed(gen_imgs),
+            np.array([[0, 0, 0], [.5, .5, .5], [1, 1, 1]])  # Grayscale palette
+        )
 
         fig, axs = plt.subplots(r, c)
         cnt = 0
@@ -164,10 +170,17 @@ class DCGAN():
                 axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
                 axs[i,j].axis('off')
                 cnt += 1
-        fig.savefig("images/mnist_%d.png" % epoch)
+        fig.savefig("images/fashion_mnist_%d.png" % epoch)
         plt.close()
 
+    def save_weights(self, g, d):
+        self.generator.save_weights(g)
+        self.discriminator.save_weights(d)
 
 if __name__ == '__main__':
     dcgan = DCGAN()
-    dcgan.train(epochs=4000, batch_size=32, save_interval=50)
+
+    try:
+        dcgan.train(epochs=4000, batch_size=32, save_interval=50)
+    finally:
+        dcgan.save_weights(g='generator.h5', d='discriminator.h5')
